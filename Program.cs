3d1f1087b8e5115d0b220775528e92af70424294
jfwqweo <119540694+uskeo@users.noreply.github.com>
2023-02-24 09:04:37 +0100

    Update for the application for logs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+
+namespace LogsApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .ConfigureLogging(loggingConfiguration => loggingConfiguration.ClearProviders())
+            .UseSerilog((hostingContext, loggerConfiguration) => 
+                loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration));
+    }
+}
